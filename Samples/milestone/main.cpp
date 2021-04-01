a605cd8f4bd2fe06fe85e599a8e0c88e573ab07c
LuongTanDat <hello.123.hello.321.hello@gmail.com>
2021-04-02 04:13:10 +0700

    Apr, 1st 2021

--- Samples/milestone/main.cpp
+++ Samples/milestone/main.cpp
@@ -5,18 +5,18 @@ simplelogger::Logger *logger = simplelogger::LoggerFactory::CreateConsoleLogger(
 int main()
 {
     ShowDecoderCapability();
-
-    // std::string szInFilePath = "/home/m/Documents/NVCODEC/your_name.mp4";
+    std::string szInFilePath = "/home/m/Documents/NVCODEC/your_name.mp4";
     // std::string szInFilePath = "rtsp://admin:esbt1234@10.236.1.105:554/onvif1";
-    std::string szInFilePath = "rtsp://admin:esbt1234@10.236.1.139:554/onvif1";
     // std::string szInFilePath = "rtsp://0.0.0.0:8554/vlc";
     // CheckInputFile(szInFilePath.c_str());
-    appDec(szInFilePath);
+
+    // AppDec(szInFilePath);
+    AppDecLowLatency(szInFilePath);
 
     return 0;
 }
 
-void appDec(std::string szInFilePath)
+void AppDec(std::string szInFilePath)
 {
     ck(cuInit(0));
     int nGpu = 0;
@@ -32,7 +32,7 @@ void appDec(std::string szInFilePath)
     uint8_t *pVideo = NULL, **ppFrame;
     cv::Mat mat_bgr;
     std::vector<std::string> aszDecodeOutFormat = {"NV12", "P016", "YUV444", "YUV444P16"};
-    cv::namedWindow("a", cv::WINDOW_NORMAL);
+    cv::namedWindow("a", cv::WINDOW_AUTOSIZE);
     do
     {
         demuxer.Demux(&pVideo, &nVideoBytes);
@@ -59,4 +59,76 @@ void appDec(std::string szInFilePath)
 
     ck(cuCtxDestroy(cuContext));
     LOG(INFO) << "End of process";
+}
+
+void AppDecLowLatency(std::string szInFilePath)
+{
+    try
+    {
+        ck(cuInit(0));
+        int nGpu = 0;
+        int iGpu = 0;
+        ck(cuDeviceGetCount(&nGpu));
+        if (iGpu < 0 || iGpu >= nGpu)
+        {
+            std::ostringstream err;
+            err << "GPU ordinal out of range. Should be within [" << 0 << ", " << nGpu - 1 << "]" << std::endl;
+            throw std::invalid_argument(err.str());
+        }
+
+        CUcontext cuContext = NULL;
+        createCudaContext(&cuContext, iGpu, 0);
+
+        FFmpegDemuxer demuxer(szInFilePath.c_str());
+        // Here set bLowLatency=true in the constructor.
+        // Please don't use this flag except for low latency, it is harder to get 100% utilization of
+        // hardware decoder with this flag set.
+        NvDecoder dec(cuContext, false, FFmpeg2NvCodecId(demuxer.GetVideoCodec()), NULL, false);
+
+        int nVideoBytes = 0, nFrameReturned = 0, nFrame = 0, n = 0;
+        uint8_t *pVideo = NULL, **ppFrame;
+        int64_t *pTimestamp;
+        cv::Mat mat_bgr;
+        std::vector<std::string> aszDecodeOutFormat = {"NV12", "P016", "YUV444", "YUV444P16"};
+        cv::namedWindow("a", cv::WINDOW_NORMAL);
+
+        do
+        {
+            demuxer.Demux(&pVideo, &nVideoBytes);
+            // Set flag CUVID_PKT_ENDOFPICTURE to signal that a complete packet has been sent to decode
+            dec.Decode(pVideo, nVideoBytes, &ppFrame, &nFrameReturned, CUVID_PKT_ENDOFPICTURE, &pTimestamp, n++);
+            if (!nFrame && nFrameReturned)
+            {
+                LOG(INFO) << dec.GetVideoInfo();
+                LOG(INFO) << "Output format: " << aszDecodeOutFormat[dec.GetOutputFormat()];
+            }
+            if (nFrameReturned < 1)
+                continue;
+            nFrame += nFrameReturned;
+            cv::Mat mat_yuv = cv::Mat(dec.GetHeight() * 1.5, dec.GetWidth(), CV_8UC1, ppFrame[nFrameReturned - 1]);
+            cv::Mat mat_rgb = cv::Mat(dec.GetHeight(), dec.GetWidth(), CV_8UC3);
+            cv::cvtColor(mat_yuv, mat_rgb, cv::COLOR_YUV2BGR_NV21);
+            cv::cvtColor(mat_rgb, mat_bgr, cv::COLOR_RGB2BGR);
+
+            cv::imshow("a", mat_bgr);
+            char c = (char)cv::waitKey(25);
+            if (c == 27)
+                break;
+
+            // For a stream without B-frames, "one in and one out" is expected, and nFrameReturned should be always 1 for each input packet
+            LOG(INFO) << "Decode: nVideoBytes=" << std::setw(10) << nVideoBytes
+                      << ", nFrameReturned=" << std::setw(10) << nFrameReturned
+                      << ", total=" << std::setw(10) << nFrame;
+            for (int i = 0; i < nFrameReturned; i++)
+            {
+                LOG(INFO) << "Timestamp: " << pTimestamp[i];
+            }
+
+        } while (nVideoBytes);
+    }
+    catch (const std::exception &ex)
+    {
+        LOG(ERROR) << ex.what();
+        exit(1);
+    }
 }
--- Samples/milestone/main.hpp
+++ Samples/milestone/main.hpp
@@ -1,3 +1,11 @@
+#pragma once
+
+#ifdef VIDEO_STREAMER_EXPORTS
+#define VIDEO_STREAMER_API __declspec(dllexport)
+#else
+#define VIDEO_STREAMER_API __declspec(dllimport)
+#endif
+
 #include <iostream>
 #include <algorithm>
 #include <thread>
@@ -5,10 +13,12 @@
 #include "NvCodec/NvDecoder/NvDecoder.h"
 #include "Utils/NvCodecUtils.h"
 #include "Utils/FFmpegDemuxer.h"
+#include "Utils/ColorSpace.h"
 #include "Common/AppDecUtils.h"
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <vector>
 #include <string.h>
 
-void appDec(std::string);
+void AppDec(std::string);
+void AppDecLowLatency(std::string);
