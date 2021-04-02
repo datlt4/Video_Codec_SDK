e55feb7260223a04fe43677a7fe512e39e83cb19
LuongTanDat <hello.123.hello.321.hello@gmail.com>
2021-04-02 13:10:41 +0700

    Apr, 2nd 2021

--- Samples/milestone/main.cpp
+++ Samples/milestone/main.cpp
@@ -4,15 +4,40 @@ simplelogger::Logger *logger = simplelogger::LoggerFactory::CreateConsoleLogger(
 
 int main()
 {
-    ShowDecoderCapability();
+    // ShowDecoderCapability();
+
     std::string szInFilePath = "/home/m/Documents/NVCODEC/your_name.mp4";
+    // std::string szInFilePath = "/dev/video0";
     // std::string szInFilePath = "rtsp://admin:esbt1234@10.236.1.105:554/onvif1";
+    // std::string szInFilePath = "rtsp://admin:esbt1234@10.236.1.139:554/onvif1";
     // std::string szInFilePath = "rtsp://0.0.0.0:8554/vlc";
+
     // CheckInputFile(szInFilePath.c_str());
 
     // AppDec(szInFilePath);
-    AppDecLowLatency(szInFilePath);
+    // AppDecLowLatency(szInFilePath);
 
+    VideoStreamer *streamer = new VideoStreamer(szInFilePath, 0);
+    std::thread *thread = new std::thread(&VideoStreamer::update, streamer);
+    bool state;
+    cv::Mat mat_bgr;
+    uchar *str;
+    cv::namedWindow("a", cv::WINDOW_AUTOSIZE);
+    while (true)
+    {
+        if (streamer->status())
+        {
+            streamer->read(state, mat_bgr);
+            if (state)
+            {
+                cv::imshow("a", mat_bgr);
+                char c = (char)cv::waitKey(25);
+                if (c == 27)
+                    break;
+            }
+        }
+    }
+    thread->detach();
     return 0;
 }
 
@@ -46,7 +71,7 @@ void AppDec(std::string szInFilePath)
         if (nFrameReturned < 1)
             continue;
         nFrame += nFrameReturned;
-        cv::Mat mat_yuv = cv::Mat(dec.GetHeight() * 1.5, dec.GetWidth(), CV_8UC1, ppFrame[nFrameReturned - 1]);
+        cv::Mat mat_yuv = cv::Mat(dec.GetHeight() * 3 / 2, dec.GetWidth(), CV_8UC1, ppFrame[nFrameReturned - 1]);
         cv::Mat mat_rgb = cv::Mat(dec.GetHeight(), dec.GetWidth(), CV_8UC3);
         cv::cvtColor(mat_yuv, mat_rgb, cv::COLOR_YUV2BGR_NV21);
         cv::cvtColor(mat_rgb, mat_bgr, cv::COLOR_RGB2BGR);
@@ -105,7 +130,7 @@ void AppDecLowLatency(std::string szInFilePath)
             if (nFrameReturned < 1)
                 continue;
             nFrame += nFrameReturned;
-            cv::Mat mat_yuv = cv::Mat(dec.GetHeight() * 1.5, dec.GetWidth(), CV_8UC1, ppFrame[nFrameReturned - 1]);
+            cv::Mat mat_yuv = cv::Mat(dec.GetHeight() * 3 / 2, dec.GetWidth(), CV_8UC1, ppFrame[nFrameReturned - 1]);
             cv::Mat mat_rgb = cv::Mat(dec.GetHeight(), dec.GetWidth(), CV_8UC3);
             cv::cvtColor(mat_yuv, mat_rgb, cv::COLOR_YUV2BGR_NV21);
             cv::cvtColor(mat_rgb, mat_bgr, cv::COLOR_RGB2BGR);
@@ -125,10 +150,112 @@ void AppDecLowLatency(std::string szInFilePath)
             }
 
         } while (nVideoBytes);
+        ck(cuCtxDestroy(cuContext));
+        LOG(INFO) << "End of process";
     }
     catch (const std::exception &ex)
     {
         LOG(ERROR) << ex.what();
         exit(1);
     }
+}
+
+VideoStreamer::VideoStreamer(std::string srcStr, int iGpu)
+{
+    ShowDecoderCapability();
+    this->srcStr = new std::string(srcStr);
+    this->iGpu = iGpu;
+}
+
+void VideoStreamer::update()
+{
+    try
+    {
+        ck(cuInit(0));
+        int nGpu = 0;
+        ck(cuDeviceGetCount(&nGpu));
+        if (this->iGpu < 0 || this->iGpu >= nGpu)
+        {
+            std::ostringstream err;
+            err << "GPU ordinal out of range. Should be within [" << 0 << ", " << nGpu - 1 << "]" << std::endl;
+            throw std::invalid_argument(err.str());
+        }
+
+        CUcontext cuContext = NULL;
+        createCudaContext(&cuContext, this->iGpu, 0);
+        FFmpegDemuxer demuxer(this->srcStr->c_str());
+        // Here set bLowLatency=true in the constructor.
+        // Please don't use this flag except for low latency, it is harder to get 100% utilization of
+        // hardware decoder with this flag set.
+        NvDecoder dec(cuContext, false, FFmpeg2NvCodecId(demuxer.GetVideoCodec()), NULL, false);
+
+        int nVideoBytes = 0, nFrameReturned = 0, nFrame = 0, n = 0;
+        uint8_t *pVideo = NULL, **ppFrame;
+        int64_t *pTimestamp;
+        std::vector<std::string> aszDecodeOutFormat = {"NV12", "P016", "YUV444", "YUV444P16"};
+        // cv::namedWindow("a", cv::WINDOW_NORMAL);
+        do
+        {
+            demuxer.Demux(&pVideo, &nVideoBytes);
+            // Set flag CUVID_PKT_ENDOFPICTURE to signal that a complete packet has been sent to decode
+            dec.Decode(pVideo, nVideoBytes, &ppFrame, &nFrameReturned, CUVID_PKT_ENDOFPICTURE, &pTimestamp, n++);
+            if (!nFrame && nFrameReturned)
+            {
+                LOG(INFO) << dec.GetVideoInfo();
+                LOG(INFO) << "Output format: " << aszDecodeOutFormat[dec.GetOutputFormat()];
+                this->frame_height = dec.GetHeight();
+                this->frame_width = dec.GetWidth();
+            }
+
+            if (nFrameReturned < 1)
+                continue;
+            nFrame += nFrameReturned;
+
+            cv::Mat mat_yuv = cv::Mat(dec.GetHeight() * 3 / 2, dec.GetWidth(), CV_8UC1, ppFrame[nFrameReturned - 1]);
+            cv::Mat mat_rgb = cv::Mat(dec.GetHeight(), dec.GetWidth(), CV_8UC3);
+            cv::cvtColor(mat_yuv, mat_rgb, cv::COLOR_YUV2BGR_NV21);
+            cv::cvtColor(mat_rgb, this->mat_bgr, cv::COLOR_RGB2BGR);
+            this->_status = true;
+
+        } while (nVideoBytes);
+        ck(cuCtxDestroy(cuContext));
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+        exit(1);
+    }
+}
+
+uchar *VideoStreamer::read()
+{
+    cv::Mat image;
+    this->mat_bgr.copyTo(image);
+    uchar *data = image.data;
+    return data;
+}
+
+void VideoStreamer::read(bool &s, cv::Mat &image)
+{
+    s = this->_status;
+    this->mat_bgr.copyTo(image);
+}
+
+bool VideoStreamer::status()
+{
+    return this->_status;
+}
+
+int VideoStreamer::getWidth()
+{
+    return this->frame_width;
+}
+
+int VideoStreamer::getHeight()
+{
+    return this->frame_height;
+}
+
+VideoStreamer::~VideoStreamer()
+{
 }
--- Samples/milestone/main.hpp
+++ Samples/milestone/main.hpp
@@ -20,5 +20,58 @@
 #include <vector>
 #include <string.h>
 
+
+
 void AppDec(std::string);
 void AppDecLowLatency(std::string);
+
+class VideoStreamer
+{
+
+public:
+    VideoStreamer(std::string, int);
+    ~VideoStreamer();
+
+    void update();
+    uchar *read();
+    void read(bool &, cv::Mat &);
+    bool status();
+    int getWidth();
+    int getHeight();
+
+private:
+    std::string *srcStr;
+    int iGpu = 0;
+
+    volatile bool _status = false;
+    int frame_width, frame_height;
+    cv::Mat mat_bgr;
+};
+
+// extern "C" VIDEO_STREAMER_API VideoStreamer *VideoStreamer_new();
+// extern "C" VIDEO_STREAMER_API void init(VideoStreamer *);
+// extern "C" VIDEO_STREAMER_API void newStrSource(VideoStreamer *);
+// extern "C" VIDEO_STREAMER_API void update(VideoStreamer *);
+// extern "C" VIDEO_STREAMER_API const char *read(VideoStreamer *);
+// extern "C" VIDEO_STREAMER_API bool status(VideoStreamer *);
+// extern "C" VIDEO_STREAMER_API void kill_stream(VideoStreamer *);
+// extern "C" VIDEO_STREAMER_API bool empty(VideoStreamer *);
+
+// extern "C"
+// {
+//     VideoStreamer *VideoStreamer_new() { return new VideoStreamer(); }
+//     void init(VideoStreamer *stream) { stream->init(); }
+//     void newStrSource(VideoStreamer *stream) { stream->newStrSource(); }
+//     void update(VideoStreamer *stream) { stream->update(); }
+//     const char *read(VideoStreamer *stream) { return stream->read(); }
+//     bool status(VideoStreamer *stream) { return stream->status(); }
+//     bool empty(VideoStreamer *stream) { return stream->empty(); }
+//     void kill_stream(VideoStreamer *stream)
+//     {
+//         if (stream)
+//         {
+//             delete stream;
+//             stream = nullptr;
+//         }
+//     }
+// }
